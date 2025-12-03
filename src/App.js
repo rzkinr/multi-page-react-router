fbd85de14092c0b55f77df774ecb402775065790
rzkinr <rizkynurrokhim18@gmail.com>
2025-12-03 12:03:53 +0700

    add root layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductPage from './pages/Products';
+import RootLayout from './pages/Root';
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductPage /> },
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductPage /> },
+    ],
+  },
 ]);
 
 function App() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>
+        Go to <Link to='/products'>Products Page</Link>
+      </p>
+    </>
+  );
 }
 
 export default HomePage;
